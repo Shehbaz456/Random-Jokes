718d78ec58ff160f052891bf31134b8329334b6f
Shehbaz <106955650+Shehbaz456@users.noreply.github.com>
2023-09-14 16:59:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,537 @@
+
+// // get the joke section container
+// // const jokeContainer = document.getElementById("joke");
+// // get the generate joke button
+// // const btn = document.getElementById("btn");
+
+// `// const url = "https://v jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single";
+// `,
+// `
+// // let getjoke = () =>{`
+// //     fetch(url)
+// //     .then(data => data.json())
+// //     .then( item =>{
+// //     jokeContainer.textContent = `${item.joke}`;
+// //     });
+// // }  
+
+// `
+// /,/ btn.addEventListener("click",getjoke);`
+// // getjoke();
+// //  console.log(item.joke)
+
+// `
+// /,/ --------------------------------------------------------------`
+
+// `
+// `,
+// `// // const jokeContainer = document.getElementById("joke");
+// // // const btn = document.getElementById("btn");`
+
+// `// let jokes =
+// // []`,
+
+
+// // a.forEach(x => {
+// //     let x = Math.floor((Math.random() *   ) +  );
+// //     console.log(x)
+// // });
+
+
+// // const btn = document.getElementById("btn");
+
+// // btn.addEventListener("click", () => {
+// //     let x = Math.floor(Math.random() * jokes.length);
+// //     document.getElementById("joke").innerHTML = jokes[x];
+// // })
+
+
+// ----------------------------------------------------
+
+
+let jokes_btn = document.getElementById("jokes_btn")
+let joke = document.getElementById("joke")
+let jokes= [
+
+    `Why did an, old man fall in a well?
+     Because he couldn’t see that well!`,
+    
+     `Why did the actor fall through the floorboards?
+      They were going through a stage!`,
+    
+     `Why did a scarecrow win a Nobel prize?
+     He was outstanding in his field!`,
+    
+     `Why are peppers the best at archery?
+     Because they habanero!`,
+    
+    `What did the duck say after she bought chapstick?
+     Put it on my bill!`,
+    
+     `What do you call a fake noodle?
+     An impasta!`,
+    
+    `What did the three-legged dog say when he walked into a saloon?
+    “I’m looking for the man who shot my paw!”`,
+    
+     `How do you tell the difference between a bull and a cow?
+     It is either one or the udder!`,
+    
+    `What’s red and smells like blue paint?
+     Red paint!`,
+    
+     `What’s the difference between a hippo and a Zippo?
+     One is very heavy, the other is a little lighter!`,
+
+    
+` What do you call a boomerang that won’t come back?
+A stick.`,
+
+` What does a cloud wear under his raincoat?
+Thunderwear.`,
+
+` Two pickles fell out of a jar onto the floor. What did one say to the other?
+Dill with it.`,
+
+` What time is it when the clock strikes   ?
+Time to get a new clock.`,
+
+`  How does a cucumber become a pickle?
+It goes through a jarring experience.`,
+
+`   What did one toilet say to the other?
+You look a bit flushed.`,
+
+`    What do you think of that new diner on the moon?
+Food was good, but there really wasn’t much atmosphe,re.`,
+
+`   Why did the dinosaur cross the road?
+Because the chicken wasn’t born yet.`,
+
+`    Why can’t Elsa from Frozen have a balloon?
+Because she will "let it go, let it go."`,
+
+`   What musical instrument is found in the bathroom?
+A tuba toothpaste.`,
+
+
+`  . Why did the kid bring a ladder to school?
+Because she wanted to go to high school.`,
+
+`   What do you call a dog magician?
+A labracadabrador.`,
+
+`   Where would you find an elephant?
+The same place you lost her.`,
+
+`  How do you get a squirrel to like you?
+Act like a nut.`,
+
+`   What do you call two birds in love?
+Tweethearts`,
+
+`     How are false teeth like stars?
+They come out at night.`,
+
+`    What building in your town has the most stories?
+The public library.`,
+
+`     What’s worse than finding a worm in your apple?
+Finding half a worm.`,
+
+
+` What did one volcano say to the other?
+I lava you.`,
+
+` How do we know that the ocean is friendly?
+It waves.`,
+
+` What is a tornado’s favorite game to play?
+Twister.`,
+
+
+` How do you talk to a giant?
+Use big words.`,
+
+
+
+` What animal is always at a baseball game?
+A bat.`,
+
+`   What falls in winter but never gets hurt?
+Snow.`,
+
+`What did the Dalmatian say after lunch?
+That hit the spot.`,
+
+
+
+`   What do you call a droid that takes the long way around?
+R2 detour.`,
+
+`  . Why did the cookie go to the hospital?
+Because he felt crummy.`,
+
+`   Why was the baby strawberry crying?
+Because her mom and dad were in a jam.`,
+
+
+`  How do you make a lemon drop?
+Just let it fall.`,
+
+`   What did the limestone say to the geologist?
+Don’t take me for granite.`,
+
+`    Why does a seagull fly over the sea?
+Because if it flew over the bay, it would, be a baygull.`,
+
+`     What kind of water can’t freeze?
+Hot water.`,
+
+`    What kind of tree fits in your hand?
+A palm tree.`,
+
+`     What do you call a dinosaur that is sleeping?
+A dino-snore.`,
+
+` What is fast, loud and crunchy?
+A rocket chip.`,
+
+`   Why did the teddy bear say no to dessert?
+Because she was stuffed.`,
+
+` What has ears but cannot hear?
+A cornfield.`,
+
+` What did the left eye say to the right eye?
+Between us, something smells.`,
+
+` What did one plate say to the other plate?
+Dinner is on me.`,
+
+` Why did the student eat his homework?
+Because the teacher told him it was a piece of cake.`,
+
+`  When you look for something, why is it always in the last place you look?
+Because when you find it, you stop looking.`,
+
+`   What is brown, hairy and wears sunglasses?
+A coconut on vacation.`,
+
+`  What do you say to a rabbit on its birthday?
+Hoppy Birthday.`,
+
+`   What’s the one thing will you get every year on your birthday, guaranteed?
+A year older.`,
+
+`   Why do candles always go on the top of cakes?
+Because it's hard to light them from the bottom.`,
+
+`  . What do cakes and baseball teams have in common?
+They both need a good batter.`,
+
+`   What goes up but never comes down?
+Your age.`,
+
+`   What does every birthday end with?
+The letter Y.`,
+
+`  What did the tiger say to her cub on his birthday?
+It's roar birthday.`,
+
+`   Why did the girl put her cake in the freezer?
+She wanted to ice it.`,
+
+`    Does a green candle burn longer than a pink one?
+No, they both burn shorter.`,
+
+`     Why did the little girl hit her birthday cake with a hammer?
+It was a pound cake.`,
+
+`  Why didn't the quarter roll down the hill with the nickel?
+Because it had more cents.`,
+
+`   Why is the obtuse triangle always so frustrated?
+Because it’s never right.`,
+
+`    Why is six afraid of seven?
+Because seven eight nine.`,
+
+
+`     How do you stay warm in any room?
+Go to the corner—it’s always    degree,s.`,
+
+`   Why was the fraction nervous about marrying the decimal?
+Because he would have to convert.`,
+
+`   Are monsters good at math?
+Not unless you count Dracula.`,
+
+`   Why was the math book sad?
+Because it had too many problems.`,
+
+`   Why does nobody talk to circles?
+Because there’s no point.`,
+
+
+`   What was the first animal in space?
+The cow that jumped over the moon.`,
+
+`    Why don’t elephants chew gum?
+They do, just not in public.`,
+
+`     What did the banana say to the dog?
+Bananas can’t talk.`,
+
+
+
+`     What do you call a sleeping bull?
+A bull-dozer.`,
+
+`  How do you fit more pigs on a farm?
+Build a sty-scraper.`,
+
+` . What did the farmer call the cow that had no milk?
+An udder failure.`,
+
+`   Why do fish live in salt water?
+Because pepper makes them sneeze.`,
+
+` What do you get from a pampered cow?
+Spoiled milk.`,
+
+`  Where do polar bears vote?
+The North Poll`,
+
+
+`    Why did the snake cross the road?
+To get to the other ssside.`,
+
+`   Why are fish so smart?
+Because they live in school,s.`,
+
+
+`   Why don’t pirates shower before they walk the plank?
+Because they’ll just wash up on shore later.`,
+
+`   What happened when Bluebeard fell overboard in the Red Sea?
+He got marooned.`,
+
+`   How did the pirate get his flag so cheaply?
+He bought it on sail.`,
+
+`   What has  legs,  arms, and  eyes?
+ pirates.`,
+
+`  How much does it cost a pirate to get his ears pierced?
+About a buck an ear.`,
+
+`   Why is pirating so addictive?
+They say once ye lose yer first ha,nd, ye get hooked.`,
+
+`    How do pirates know that they are pirates?
+They think, therefore they arrr.`,
+
+
+`    What song does a cat like best?
+Three Blind Mice.`,
+
+`     Where did the school kittens go for their field trip?
+To the mew-seum.`,
+
+`    What kind of kitten works for the Red Cross?
+A first-aid kit.`,
+
+`   Why are cats good at video games?
+Because they have nine lives.`,
+
+`    What did the cat say when he fell off the table?
+"Me-ow."`,
+
+`    What is the difference between a cat that got photocopied and a cat that follows you?
+One is a cat copy; the other is a copy cat.`,
+
+`   What do you get when you cross a ball and a cat?
+A fur ball.`,
+
+`    What's a cat's favorite magazine?
+A cat-alogue.`,
+
+`     What cat likes living in water?
+An octo-puss.`,
+
+`  What key won't open any door?
+A turkey.`,
+
+`   Why did the turkey cross the road?
+It was the chicken's day off.`,
+
+`    Why did the chewing gum cross the road?
+It was stuck on the turkey's foot.`,
+
+`   Why did the turkey cross the road twice?
+To show he wasn't a chicken.`,
+
+`    What do you get when a turkey lays an egg on top of a barn?
+An eggroll.`,
+
+`Why was the turkey the drummer in the band?
+Because he had drumsticks.`,
+
+`What’s the best thing to put into pumpkin pie?
+Your teeth.`,
+
+`What’s the best dance to do on Thanksgiving?
+The turkey trot.`,
+
+`Why did the Pilgrims sail from England to America?
+Because they missed their plane.`,
+
+`When the Pilgrims landed, where did they stand?
+On their feet.`,
+
+`Why did the police arrest the turkey?
+They suspected it of fowl play.`,
+
+`What should you wear to Thanksgiving dinner?
+A har-vest.`,
+
+`If the Pilgrims were alive today, what would they be most famous for?
+Their age.`,
+
+`Where do you find a turkey with no legs?
+Where you left it.`,
+
+`What do you call it when it rains turkeys?
+Foul weather.`,
+
+`Why did the turkey sit on the tomahawk?
+To hatch-et.`,
+
+`What kind of music did Pilgrims listen to?
+Plymouth Rock.`,
+
+`What smells the best at a Thanksgiving dinner?
+Your nose.`,
+
+`Why do turkeys always say, “gobble, gobble”?
+Because they never learned good table manners.`,
+
+`What do elves learn in school?
+The elf-abet.`,
+
+
+`How does a snowman lose weight?
+He waits for the weather to get warmer.`,
+
+`What did one snowman say to the other snowman?
+Do you smell carrots?`,
+
+`What do you call a reindeer with bad manners?
+Rude-olph.`,
+
+`Why does Santa work at the North Pole?
+Because the penguins kicked him out of the S,outh Pole.`,
+
+`What falls at the North Pole and never gets hurt?
+Snow.`,
+
+`How does a sheep say Merry Christmas?
+Fleece Navidad.`,
+
+`Why did the Christmas tree go to the barber?
+It needed to be trimmed.`,
+
+`What is an elf’s favorite kind of music?
+Wrap music.`,
+
+`What kind of photos do elves take?
+Elfies.`,
+
+`What do road crews use at the North Pole?
+Snow cones.`,
+
+`Why did Rudolph get a bad grade on his report card?
+Because he went down in history.`,
+
+`What wears a red suit and goes, “Oh, oh, oh”?
+Santa walking backwards.`,
+
+`Where does a snowman keep his money?
+In a snow bank.`,
+
+`What do you get when you combine a Christmas tree with a computer?
+A pine-apple.`,
+
+`In what year does New Year’s Day come before Christmas?
+Every year.`,
+
+`What do snowmen eat for breakfast?
+Frosted Flakes.`,
+
+`What kind of motorcycle does Santa like to ride?
+A Holly Davidson.`,
+
+`Why are Christmas trees bad at sewing?
+Because they always drop their needles.`,
+
+`What do you get when Santa becomes a detective?
+Santa clues.`,
+
+`  Why was the Easter Bunny so upset?
+He was having a bad hare day.`,
+
+`   How did the soggy Easter Bunny dry himself?
+With a hare dryer.`,
+
+`    How does the Easter bunny stay in shape?
+Lots of eggs-ercise.`,
+
+`     Why can’t a rabbit’s nose be  2 inches long?
+Because then it would be a foot.`,
+
+`    How can you tell which rabbits are the oldest in a group?
+Just look for the gray hares.`,
+
+`     What do you call a bunny who isn’t smart?
+A hare brain.`,
+
+`    What’s the best way to catch a unique rabbit?
+Unique up on him.`,
+
+`   . What happened when the Easter Bunny met the rabbit of his dreams?
+They lived hoppily ever after.`,
+
+` What do you call a line of rabbits walking backwards?
+A receding hareline.`,
+
+`Why shouldn’t you tell an Easter egg a good joke?
+It might crack up.`,
+
+`What did one colored egg say to the other?
+Heard any good yolks lately?`,
+
+`What do you call a rabbit who tells jokes?
+A funny bunny.`,
+
+`What is a rabbit’s favorite dance?
+The Bunny Hop.`,
+
+` What kind of jewelry do rabbits wear?
+   carrot gold.`,
+
+` How many chocolate bunnies can you put into an empty Easter basket?
+Only one because after that, it’s not empty.`,
+
+`What do you call a rabbit with fleas?
+Bugs Bunny.`,
+
+` Why did the Easter egg hide?
+He was a little chicken.`
+]
+
+jokes_btn.addEventListener("click", myFunction);
+function myFunction(){
+    let random =Math.floor((Math.random()* jokes.length )+ 1)
+    joke.innerHTML = jokes[random];
+}
